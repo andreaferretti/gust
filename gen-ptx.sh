fdf5b473397959cee7b06792341d5fbaa5dc2287
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-07-13 17:20:37 +0200

    minor

--- gen-ptx.sh
+++ gen-ptx.sh
@@ -1,5 +1,3 @@
-PATH="$PATH:/Developer/NVIDIA/CUDA-5.5/bin/"
-
 if [ `uname` == Darwin ]; then
    NVCC_OPTS="-gencode arch=compute_30,code=sm_30 -ccbin /usr/bin/llvm-g++-4.2 $NVCC_OPTS"
 fi
@@ -17,4 +15,3 @@ nvcc $NVCC_OPTS -D TYPE=float -I src/main/resources/gust/linalg/cuda/ --ptx src/
 
 nvcc $NVCC_OPTS -D TYPE=double -I src/main/resources/gust/linalg/cuda/ --ptx src/main/resources/gust/linalg/cuda/reduceFloat.cu -o src/main/resources/gust/linalg/cuda/reduceFloat.ptx
 nvcc $NVCC_OPTS -D TYPE=float -I src/main/resources/gust/linalg/cuda/ --ptx src/main/resources/gust/linalg/cuda/reduceDouble.cu -o src/main/resources/gust/linalg/cuda/reduceDouble.ptx
-
